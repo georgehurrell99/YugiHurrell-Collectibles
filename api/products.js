79b04b2cbdb082ed5ce556307ecad41c4f93c0d1
georgehurrell99 <georgehurrell99@gmail.com>
2026-09-01 18:58:02 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -3,12 +3,41 @@ export default async function handler(req, res) {
   const clientId = process.env.SHOPIFY_CLIENT_ID;
   const clientSecret = process.env.SHOPIFY_CLIENT_SECRET;
 
-  return res.status(200).json({
-    shop,
-    shopUrl: `https://${shop}.myshopify.com`,
-    hasClientId: !!clientId,
-    hasClientSecret: !!clientSecret,
-    clientIdLength: clientId ? clientId.length : 0,
-    clientSecretLength: clientSecret ? clientSecret.length : 0,
-  });
+  const tokenUrl =
+    `https://${shop}.myshopify.com/admin/oauth/access_token`;
+
+  try {
+    const response = await fetch(tokenUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret,
+      }),
+    });
+
+    const responseText = await response.text();
+
+    return res.status(200).json({
+      tokenUrl,
+      status: response.status,
+      statusText: response.statusText,
+      response: responseText,
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      tokenUrl,
+      error: error.message,
+      cause: error.cause
+        ? {
+            message: error.cause.message,
+            code: error.cause.code,
+          }
+        : null,
+    });
+  }
 }
